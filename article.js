9ee76157fad4d84aa52622e4db8c3d8161efb070
Aditya Gupta <144304354+Aditya21Gupta@users.noreply.github.com>
2024-06-25 22:01:49 +0530

    Add files via upload

---
+++ article.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const marked = require('marked');
+const slugify = require('slugify');
+const { JSDOM } = require('jsdom');
+
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    markdown: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+articleSchema.pre('validate', function(next) {
+    if (this.title) {
+        this.slug = slugify(this.title, { lower: true, strict: true });
+    }
+
+    if (this.markdown) {
+        const dom = new JSDOM(marked(this.markdown));
+        this.description = dom.window.document.querySelector('p').textContent.trim();
+    }
+
+    next();
+});
+
+module.exports = mongoose.model('Article', articleSchema);
